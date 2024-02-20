3fab96c2aa030f635679236104c7cc3721d3adf5
Farkas Levente <farkas.levente2004@gmail.com>
2024-02-20 10:09:23 +0100

    v3: Dynamic table creation is finally working

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,19 +1,158 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import { useStore, IMany } from "../stores/store";
+import { onMounted } from "vue";
+import { QTableColumn } from "quasar";
+import NewDialogComponent from "../components/NewDialogComponent.vue";
+import EditDialogComponent from "../components/EditDialogComponent.vue";
+
+const slide = ref(1);
+const store = useStore();
+
+function onItemClick() {}
+const columns: QTableColumn[] = [
+  { name: "id", label: "Azon", field: "id", align: "left", sortable: true },
+  { name: "titleField", label: "Cím", field: "titleField", align: "left", sortable: true },
+  {
+    name: "descField",
+    label: "Leírás",
+    field: (row: IMany) => {
+      const desc: string = row.descField as string;
+      if (desc.length > 130) {
+        return desc.slice(0, 124) + "...";
+      } else {
+        return desc;
+      }
+    },
+    align: "left",
+    sortable: true,
+  },
+  {
+    name: "categoryNameField",
+    label: "Kategória",
+    field: (row: IMany) => row.category?.categoryNameField,
+    align: "left",
+    sortable: true,
+  },
+  { name: "imgField", label: "Kép", field: "imgField", align: "center" },
+  { name: "boolField", label: "Tehermentes", field: "boolField", align: "center" },
+];
+
+onMounted(() => {
+  store.many_GetAll();
+});
+
+function deleteDocument(): void {
+  store.many.document = { id: store.app.selectedMany[0].id };
+  store.many_DeleteById();
+  // selected.value = [];
+  store.app.selectedMany = [];
+}
+
+function editDocument() {
+  store.many.document.id = store.app.selectedMany[0].id;
+  store.app.showEditDialog = true;
+}
+
+// function filterUpdate() {
+//   if (!store.app.filter) {
+//     store.app.filter = "";
+//   }
+//   if (store.app.filter.length > 0) {
+//     store.many_Filter();
+//   } else {
+//     store.many_GetAll();
+//   }
+// }
+</script>
 
 <template>
   <q-page>
+    <div class="q-pa-md column items-center justify-start">
+      <q-btn-dropdown color="gray" label="Dropdown Button" text-color="black">
+        <q-list>
+          <q-item v-close-popup clickable @click="onItemClick">
+            <q-item-section>
+              <q-item-label>Photos</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-item v-close-popup clickable @click="onItemClick">
+            <q-item-section>
+              <q-item-label>Videos</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-item v-close-popup clickable @click="onItemClick">
+            <q-item-section>
+              <q-item-label>Articles</q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-btn-dropdown>
+    </div>
     <div class="column flex-center" style="min-height: inherit">
       <img alt="Jedlik logo" src="../assets/Jedlik_big.png" />
       <h2>Jedlik Ányos Technikum - Győr</h2>
     </div>
+    <div class="row">
+      <div class="col col-lg-12 col-md-4 col-sm-4"></div>
+    </div>
     <q-card>
-      
+      <div class="q-pa-md">
+        <q-carousel v-model="slide" animated infinite swipeable thumbnails>
+          <q-carousel-slide img-src="https://cdn.quasar.dev/img/mountains.jpg" :name="1" />
+          <q-carousel-slide img-src="https://cdn.quasar.dev/img/parallax1.jpg" :name="2" />
+          <q-carousel-slide img-src="https://cdn.quasar.dev/img/parallax2.jpg" :name="3" />
+          <q-carousel-slide img-src="https://cdn.quasar.dev/img/quasar.jpg" :name="4" />
+        </q-carousel>
+      </div>
     </q-card>
+    <NewDialogComponent />
+    <EditDialogComponent />
+    <q-table
+      v-model:selected="store.app.selectedMany"
+      card-class="bg-indigo-9 text-white"
+      card-style="padding: 22px;"
+      :columns="columns"
+      grid
+      row-key="id"
+      :rows="store.many.documents"
+      selection="multiple"
+      title="Hahu hirdetések"
+    />
+    <div class="row justify-center q-mt-sm q-gutter-md">
+      <q-btn color="green" label="Új hirdetés" no-caps @click="store.app.showNewDialog = true" />
+      <q-btn
+        v-show="store.app.selectedMany.length == 1"
+        color="red"
+        label="Hirdetés törlése"
+        no-caps
+        @click="deleteDocument()"
+      />
+      <q-btn
+        v-show="store.app.selectedMany.length == 1"
+        color="red"
+        label="Hirdetés szerkesztése"
+        no-caps
+        @click="editDocument()"
+      />
+      <q-btn
+        v-show="store.app.selectedMany.length != 0"
+        color="green"
+        label="Kijelölés törlése"
+        no-caps
+        @click="store.app.selectedMany = []"
+      />
+    </div>
   </q-page>
 </template>
 
 <style lang="scss" scoped>
 h2 {
   font-size: 3vw;
+}
+#anyu {
+  justify-content: space-evenly;
 }
 </style>
